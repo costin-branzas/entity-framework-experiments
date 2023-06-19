10e60a8b46546dae7a3a6e55da58e9044f33ad5a
Costin Branzas <costin.branzas@qubiz.com>
2023-06-19 11:57:14 +0300

    adding factory

--- EntityFrameWorkRelations/Program.cs
+++ EntityFrameWorkRelations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -113,5 +115,21 @@ class BrickContext : DbContext
         modelBuilder.Entity<BasePlate>().HasBaseType<Brick>();
         modelBuilder.Entity<MinifigHead>().HasBaseType<Brick>();
     }
+}
 
+class BrickContextFactory : IDesignTimeDbContextFactory<BrickContext>
+{
+    public BrickContext CreateDbContext(string[]? args = null)
+    {
+        var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<BrickContext>();
+        optionsBuilder
+            // Uncomment the following line if you want to print generated
+            // SQL statements on the console.
+            // .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
+            .UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+
+        return new BrickContext(optionsBuilder.Options);
+    }
 }
